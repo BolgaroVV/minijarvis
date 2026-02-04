ab67426528d40fea533b8ccdbfbedb7c1bbff119
bolgarovvv <khasanov.alexey999@gmail.com>
2026-02-04 16:33:52 +0500

    updates in yandex_api

--- yandex_api.py
+++ yandex_api.py
@@ -1,53 +1,125 @@
-from yandex_music import Client, Track
-from pygame_player import AudioPlayer, controls
-from stt import STT
+from yandex_music import Track, Album
 import asyncio
-import time
 import threading
+from services import container
 
-# 113794712
-# 5535519
-client = Client('y0__xCpmpWuARje-AYg2_7smhYpIYKyuuCA7_QXlcnhWyFwl22cHw')
-client.init()
+class MusicControls():
+    def __init__(self):
+        self._load_id = 0
+        self._lock = threading.Lock()
+        self.thread = None
+        self.stop_event = threading.Event()
+    def start_loader(self, loader, *args):
+        print(threading.active_count())
+        print(threading.enumerate())
+        def wrapper():     
+            with self._lock:
+                loader(*args)
+        self.thread = threading.Thread(target=wrapper, daemon=True)
+        self.thread.start()
 
-def get_download_link(track: Track) -> str:
-    track_download_info = track.get_download_info(1)[0]
-    return track_download_info['direct_link']
+    # Получение информации по текущему треку
+    @property
+    def _current_track(self):
+        return container.player.current_track[1]
+    
+    def _current_track_info(self):
+        return container.client.tracks(self._current_track['id'])[0]
 
-def get_album_tracks(album_id: int):
-    album = client.albums_with_tracks(album_id)
-    album_tracks = album['volumes'][0]
-    return album_tracks
+    def _current_album(self):
+        return self._current_track_info()['albums'][0]['id']
+    
+    def _current_artists(self):
+        return [artist['id'] for artist in self._current_track_info()['artists']]
+    
+    # Контроллеры плеера без привязки к текущему треку
+    def play_next_track(self):
+        container.player.next()
+    def pause_player(self):
+        container.player.pause()
+    def resume_player(self):
+        container.player.resume()
+    def start_player(self):
+        self.start_loader(self.play_chart)
+    def play_previous_track(self):
+        container.player.previous()
 
-def voice_controls(text: str):
-    cmd = text.strip().lower()
-    if cmd == "следующий":
-        player.next()
-    elif cmd == "пауза":
-        player.pause()
-    elif cmd == "вернуть":
-        player.resume()
-    elif cmd == "старт":
-        threading.Thread(
-            target=run_load_tracks,
-            daemon=True
-        ).start()
-    # elif cmd.startswith("громкость"):
-    #     vol = float(input("Введите значение громкости от 0.0 до 1.0: "))
-    #     player.set_volume(vol)      
+    # Лайки дизлайки текущего трека/альбома/артиста
+    def like_current_track(self):
+        container.client.users_likes_tracks_add(self._current_track['id'])
+    def unlike_current_track(self):
+        container.client.users_likes_tracks_remove(self._current_track['id'])
+    def dislike_current_track(self):
+        container.client.users_dislikes_tracks_add(self._current_track['id'])
+    def undislike_current_track(self):
+        container.client.users_dislikes_tracks_remove(self._current_track['id'])
+    def like_current_album(self):
+        container.client.users_likes_albums_add(self._current_album())
+    def unlike_current_album(self):
+        container.client.users_likes_albums_remove(self._current_album())
+    def like_current_artist(self):
+        container.client.users_likes_artists_add(self._current_artists())
+    def unlike_current_artist(self):
+        container.client.users_likes_artists_remove(self._current_artists())
 
-async def load_tracks():
-    for track in get_album_tracks(25713686):
-        url = get_download_link(track)
-        player.add_to_queue(url, track)
+    # Добавить трек в конец очереди
+    def add_track_to_queue(self, track: Track = container.client.tracks(34608)[0]):
+        self.start_loader(lambda: asyncio.run(self.load_track(track)))
 
-def run_load_tracks():
-    asyncio.run(load_tracks())
+    # Включить трек после текущего
+    def play_next_track_after_current(self, track: Track = container.client.tracks(34608)[0]):
+        self.stop_event.set()
+        self.thread.join()  
+        container.player.clear_queue()
+        self.add_track_to_queue(track)
 
-stt = STT()
-stt.start(voice_controls)
-player = AudioPlayer()
+    # Включить трек прямо сейчас
+    def play_track_right_now(self, track: Track = container.client.tracks(34608)[0]):
+        self.play_next_track_after_current(track)
+        container.player.next()
 
-while True:
-    print('working')
-    time.sleep(1)
+    
+    def get_download_link(self, track: Track) -> str:
+        track_download_info = track.get_download_info(1)[0]
+        return track_download_info['direct_link']
+    def get_album_tracks(self, album_id: int):
+        album = container.client.albums_with_tracks(album_id)
+        album_tracks = album['volumes'][0]
+        return album_tracks
+    def play_chart(self):
+        chart_info = container.client.chart()
+        chart_playlist = chart_info['chart']
+        asyncio.run(self.play_playlist(chart_playlist))
+
+    async def play_playlist(self, playlist):
+        for short_track in playlist['tracks']:
+            if self.stop_event.is_set():
+                print('thread cancelled')
+                self.stop_event.clear()
+                return
+            track = short_track['track']
+            await self.load_track(track)
+
+    def start_play_album(self):
+        self.start_loader(lambda: asyncio.run(self.play_album()))
+
+    async def play_album(self):
+        album=self.get_album_tracks(40387457)
+        for track in album:
+            if self.stop_event.is_set():
+                print('thread cancelled')
+                return
+            await self.load_track(track)
+
+    async def load_track(self, track):
+        url = self.get_download_link(track)
+        container.player.add_to_queue(url, {'id': track['id'], 'title': track['title']})
+        print(f'added {track['title']}')    
+
+    def search_track(self):
+        return container.client.search(text='blod tear', type_='track', nocorrect=False)
+
+
+
+
+    
